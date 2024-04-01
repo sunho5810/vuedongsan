6738b85934b41ebf425810533f4a2454f97d2eac
Sunho Ju <sunho5515@gmail.com>
2024-04-01 22:52:14 +0900

    ModalDetail, CardProduct 컴포넌트 완성

--- src/components/ModalDetail.vue
+++ src/components/ModalDetail.vue
@@ -7,6 +7,8 @@
       <p>{{ products[currentIdx].content }}</p>
       <p>신고수 : {{ products[currentIdx].report }}</p>
       <button type="button" @click="closeModal">닫기</button>
+      <!-- ex) props 3 : props는 read-only
+        closeModal함수 형식 말고 modal = false 이런식으로 값의 재할당을 금지한다. -->
     </div>
   </div>
 </template>
@@ -14,6 +16,15 @@
 <script>
 export default {
   name: "ModalDetail",
+  /* props 2 : 부모에게서 받은 데이터를 저장한다. 
+    data명: 자료형(굳이굳이 사용할 필요는 없지만 적어주는게 좋다.)
+  */
+  props: {
+    products: Array,
+    currentIdx: Number,
+    modal: Boolean,
+    closeModal: Function,
+  }
 };
 </script>
 
