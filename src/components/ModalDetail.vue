915068b44270b3ec7e5990a3d2a48622ba6aa1b5
Sunho Ju <sunho5515@gmail.com>
2024-04-03 21:35:27 +0900

    유효성 검사 완료

--- src/components/ModalDetail.vue
+++ src/components/ModalDetail.vue
@@ -10,6 +10,9 @@
         *초기값과 받아올 값의 자료형을 잘 맞춰줘야 함
         **폼요소로 값을 받아오면 문자열로 받아오기 때문에 v-model.number와 같이 자료형을 변환해 줄 수 있다.
       -->
+      <!-- watcher 1 : 데이터를 감시하는 함수
+        특히 사용자에게서 input으로 데이터를 입력 받아 유효성 검사를 할 때 자주 사용한다.
+      -->
       <input type="text" v-model="month">
       <p>{{ month }}개월 선택함 : {{ month * products[currentIdx].price }} 만원</p>
 
@@ -30,6 +33,21 @@ export default {
   data(){
     return{
       month: 1,
+    }
+  },
+  /* 데이터를 감시할 수 있다. ex) observer */
+  watch: {
+    /* watcher 2 : 감시하고 싶은 데이터 이름으로 함수를 만들어준다.
+      파라미터는 (데이터, 변경 전 데이터)로 두개까지 받아올 수 있다.
+    */
+    month(data, before){
+      if(data >= 13){
+        alert("13이상 입력하지 마세요");
+        this.month = before;
+      } else if(typeof(data) === "string"){
+        alert("숫자만 입력해주세요.");
+        this.month = before;
+      }
     }
   },
   props: {
